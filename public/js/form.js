da480a37e7a709c3b8477580c9f08fa79ba06567
Carlos Dario <carlosdario.me@outlook.com>
2021-03-03 15:12:41 +0100

    Send files with post request
    Now the form can send files using a POST request with fetch, and the server sends a response with them.
    
    index.js is now app.js
    
    Changed app port from 8080 to 3737
    
    To avoid collisions, now in app.html the name/id "file-load" is "fileLoad", and the name/id "export" is "output".

--- public/js/form.js
+++ public/js/form.js
@@ -1,40 +1,42 @@
 'use strict';
 
+// When the HTML is fully loaded execute this code
 window.onload = () => {
-    const form = document.getElementById( "form" );
+    // When the form is submitted it cancels the operation and the download() function
+    const form = document.getElementById("form");
     form.addEventListener( "submit", (event)=>{
         event.preventDefault();
         download(form);
     } );
 
-    const files = document.getElementById( "file-load" );
-    form.addEventListener( "change", ()=>{
-        uploadedFile("file-load","file-selected");
+    // When a file is uploaded it executes the uploadedFile() function
+    const files = document.getElementById("fileLoad");
+    files.addEventListener( "change", ()=>{
+        uploadedFile("fileLoad","file-selected");
     } );
 }
 
-const uploadedFile = async (id,textId) =>{
+// Displays in the form the amount of uploaded files
+function uploadedFile(id, textId){
+    // Get the files input and the text that indicates how many files did you upload
     let files = document.getElementById(id).files;
     let text = document.getElementById(textId);
+    // It sets the text of textId to the number of files plus the string, if the files contain 0 or more than 1 object it says "files", if not it says "file"
     text.innerText = files.length>1||files.length==0 ? files.length+" files selected" : files.length+" file selected";
 }
 
-const download = async function(data){
-    const FD = new FormData(data);
-    console.log(FD.get("file-load"));
-    const sendForm = await fetch('/', {
+// Download the files
+async function download(form){
+    // Send a POST request to the server with the form
+    await sendForm(new FormData(form));
+}
+
+// Send the form
+async function sendForm(form){
+    // Fetch the form
+    const sendForm = await fetch('/upload', {
         method: 'POST',
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            user: {
-                "name": "Example",
-                "email": "example@example.com"
-            }
-        })
+        body: form
     });
-    const response = await sendForm.json();
-    console.log(response);
+    console.log(await sendForm.json());
 }
