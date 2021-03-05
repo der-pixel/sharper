8b40924064792d86c2a3c1a15d1576821d911bf4
Carlos Dario <carlosdario.me@outlook.com>
2021-03-05 17:55:41 +0100

    Return ZIP file
    Now the server takes the images and returns them in a ZIP file.
    
    The code has been also cleaned up.
    
    The README file has been corrected.

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 require('dotenv').config()
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const fileSystem = require('fs')
 const app = express();
 const sharp = require('sharp');
 const btoa = require('btoa');
+const fs = require('fs');
+const archiver = require('archiver');
 
-const { StringDecoder } = require('string_decoder');
-const decoder = new StringDecoder('utf-8');
-
-// Handle uploaded files
+// handle uploaded files
 app.use(fileUpload());
 
-// Serve these files and folders as static content
+// serve these files and folders as static content
 app.use(express.static(__dirname+'/'+process.env.STATIC_FOLDER));
 
-// Handle GET requests
+// handle GET requests
 app.get("/", (req, res) => {
    res.sendFile(__dirname + '/app.html');
 })
 
-// Handle POST requests
+// handle POST requests
 app.post('/upload', async function(req, res) {
    let files = req.files;
 
@@ -28,29 +26,57 @@ app.post('/upload', async function(req, res) {
       return res.status(400).send('No files were uploaded.');
    }
    else{
-      const object = {download: []};
+      const object = {};
       if(typeof files.fileLoad.name === "undefined"){
+         let counter = 1;
          for(element of files.fileLoad){
             await sharp(element.data)
             .withMetadata()
             .webp()
-            .toBuffer()
-            .then(data =>{
-               object.download.push('data:image/webp;base64,'+btoa(arrayBufferToString(data)));
-            });
+            .toFile('compress/'+counter+'.webp');
+            counter++;
          }
       }
       else if(typeof files.fileLoad.name === "string"){
+         // get the file and modify it
          await sharp(files.fileLoad.data)
-         .withMetadata()
-         .webp()
-         .toBuffer()
-         .then(data =>{
-            object.download.push('data:image/webp;base64,'+btoa(arrayBufferToString(data)));
-         });
+            .withMetadata()
+            .webp()
+            .toFile('compress/1.webp');
       }
-      
-      return res.status(200).json(object);
+
+      // create a file to stream archive data to.
+      const output = fs.createWriteStream(__dirname + '/images.zip');
+      const archive = archiver('zip', {
+         zlib: { level: 9 } // Sets the compression level.
+      });
+
+      // pipe archive data to the file
+      archive.pipe(output);
+
+      // append files from a sub-directory, putting its contents at the root of archive
+      archive.directory('compress/', false);
+
+      // finalize the archive (ie we are done appending files but streams have to finish yet)
+      // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+      archive.finalize();
+
+      output.on('close', function() {
+         fs.readFile(__dirname + '/images.zip', (err, data) => {
+            if (err) throw err;
+            return res.status(200).json({data:'data:application/zip;base64,'+btoa(arrayBufferToString(data))});
+         });
+         
+         //remove the files
+         console.log(archive.pointer() + ' total bytes');
+         fs.readdir("compress/", (err, files)=>{
+            files.forEach(el =>{
+               fs.promises.rm("compress/"+el);
+            })
+         });
+         // remove the zip
+         fs.promises.rm("images.zip");
+      });
    }
 });
 
