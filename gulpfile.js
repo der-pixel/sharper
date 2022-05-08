a8e85fb5ce4b3b7e505eb8273781183d37b7f025
Carlos Darío <carlosdario.me@outlook.com>
2022-05-08 19:50:28 +0200

    feat: gulpfile clean-up and modules updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 'use strict';
 const gulp = require('gulp');
-const {series} = require('gulp');
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject("tsconfig.json");
 const sass = require('gulp-sass');
@@ -8,38 +7,35 @@ sass.compiler = require('node-sass');
 const merge = require('merge-stream');
 const del = require('del');
 const uglify = require("gulp-uglify");
-const { watch } = require('gulp');
 
-// empty the build folder
-function resetGulp() {
-  return del('build/**/*', {force:true});
+function emptyBuildFolder() {
+  return del('./build/**/*', {force:true});
 };
 
-// move the static files to the destination folder
-function staticGulp() {
+function moveStaticFiles() {
   return merge(gulp.src('./src/public/fonts/**/*', {base:'./src'}), gulp.src('./src/public/images/**/*', {base:'./src'}), gulp.src('./src/**/*.html', {base:'./src'}), gulp.src('./src/public/*.png', {base:'./src'}), gulp.src('./src/public/*.xml', {base:'./src'}), gulp.src('./src/public/*.icon', {base:'./src'}), gulp.src('./src/public/*.webmanifest', {base:'./src'}))
     .pipe(gulp.dest('./build'));
 };
 
-// compile scss into css
-function sassGulp() {
+function transpileSass() {
   return gulp.src('./src/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./build'));
 };
 
-// compile typescript into javascript
-function typescriptGulp() {
+function transpileTypescript() {
     return tsProject.src()
         .pipe(tsProject()).js
         .pipe(uglify())
         .pipe(gulp.dest('./build'));
 };
 
-function watchGulp(){
-  watch('src/**/*', series(resetGulp, staticGulp, sassGulp, typescriptGulp));
+const gulpTaskSeries = gulp.series(emptyBuildFolder, moveStaticFiles, transpileSass, transpileTypescript);
+
+function gulpWatch(){
+  gulp.watch('./src/**/*', gulpTaskSeries);
 }
 
-exports.default = series(resetGulp, staticGulp, sassGulp, typescriptGulp);
+exports.default = gulpTaskSeries;
 
-exports.watch = watchGulp;
+exports.watch = gulpWatch;
